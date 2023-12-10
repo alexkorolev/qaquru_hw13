a5596b7ebf7aed0655bde37d9993a426c55d3e15
Aleksei Korolev <alexkorolev86@gmail.com>
2023-12-10 22:38:09 +0300

    add logs attachments and video

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,6 +3,9 @@ import helpers.Attach;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BaseTest {
 
@@ -13,6 +16,13 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Step("Добавление скриншотов, логов и видеофайлов")
