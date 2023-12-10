757fe6bf77676720fa3db8a4fb460a838ae5fb2c
Aleksei Korolev <alexkorolev86@gmail.com>
2023-12-10 21:48:10 +0300

    remote qa tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,5 +7,6 @@ public class BaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
