0ddd08712f6c61b599dd53f16177d23b855606d3
Aleksei Korolev <alexkorolev86@gmail.com>
2023-12-10 22:20:36 +0300

    remote qa tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,9 +1,11 @@
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
 import io.qameta.allure.Step;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
-    
+
     @Step("Настройка значений по умолчанию")
     @BeforeAll
     public static void beforeMethod(){
@@ -11,5 +13,15 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @Step("Добавление скриншотов, логов и видеофайлов")
+    @AfterEach
+    public static void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 }
